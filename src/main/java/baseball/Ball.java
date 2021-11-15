fb502e73241342050479868614be71da45c16578
autoexebat <ddingddong88@gmail.com>
2021-11-15 14:32:48 +0900

    feat: Ball 클래스 추가

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import java.util.Objects;
+
+public class Ball {
+    private int position;
+    private int number;
+
+    public Ball(int position, int number) {
+        this.position = position;
+        this.number = number;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && number == ball.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, number);
+    }
+}
